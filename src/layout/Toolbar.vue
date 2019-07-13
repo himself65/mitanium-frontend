a3571aa02e850edf42937b146224c417c4595065
himself65 <himself65@outlook.com>
2019-07-13 17:48:08 +0800

    fix: icons

--- src/layout/Toolbar.vue
+++ src/layout/Toolbar.vue
@@ -6,30 +6,11 @@
       dense
       floating
     >
-      <v-menu bottom left>
-        <v-btn
-          slot="activator"
-          icon
-        >
-          <v-badge color="red" right>
-            <span v-if="notice" slot="badge">
-              0
-            </span>
-            <v-icon>fas fa-envelope</v-icon>
-          </v-badge>
-        </v-btn>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title>暂时什么都没有</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
-
       <v-btn
         :to="userUrl"
         icon
       >
-        <v-icon>fas fa-user</v-icon>
+        <v-icon>account_circle</v-icon>
       </v-btn>
     </v-toolbar>
   </div>
@@ -38,21 +19,10 @@
 <script>
 export default {
   name: 'Toolbar',
-  data () {
-    return {
-      notice: true
-    }
-  },
   computed: {
     userUrl () {
-      if (this.$store.getters.currentUser !== null) return '/user'
-      return '/login'
+      return this.$store.getters.currentUser ? '/user' : '/login'
     }
-  },
-  mounted () {
-    setTimeout(() => {
-      this.notice = false
-    }, 2000)
   }
 }
 </script>
